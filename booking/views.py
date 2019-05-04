ff950203f9b1ec5643b98c2379cda2a4646f70a2
Jakob Damgaard Møller <jakobdo@gmail.com>
2019-05-04 15:24:23 +0200

    Booking almost done

--- booking/views.py
+++ booking/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
+from datetime import timedelta
+
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse, reverse_lazy
 from django.views import View
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from booking.forms import BookingForm
+from booking.models import Booking
 
 
-class BookingView(View):
-    template_name = 'booking.html'
+class BookingCreate(CreateView):
+    model = Booking
+    fields = ['name', 'date_from', 'date_to']
+    success_url = reverse_lazy('booking_list')
+
+
+class BookingUpdate(UpdateView):
+    model = Booking
+    fields = ['name', 'date_from', 'date_to']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('booking_list')
+
+
+class BookingDelete(DeleteView):
+    model = Booking
+    success_url = reverse_lazy('booking_list')
+
+
+class BookingListView(View):
+    template_name = 'list.html'
 
     def get(self, request):
-        form = BookingForm()
-        return render(request, self.template_name, {'form': form})
+        bookings = Booking.objects.all()
+        return render(request, self.template_name, {'bookings': bookings})
+
+
+class BookingApiView(View):
+    def get(self, request):
+        events = []
+        for booking in Booking.objects.all():
+            events.append(dict(
+                id=booking.id,
+                title=booking.name,
+                start=booking.date_from.strftime("%Y-%m-%d"),
+                end=(booking.date_to + timedelta(days=1)).strftime("%Y-%m-%d"),
+                url=reverse('booking_update', kwargs={'pk': booking.pk})
+            ))
+        return JsonResponse(events, safe=False)
--- ternevej/urls.py
+++ ternevej/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from booking.views import BookingApiView, BookingListView, BookingCreate, BookingUpdate, BookingDelete
+
 urlpatterns = [
-    path('booking/', )
+    path('', BookingListView.as_view(), name="booking_list"),
+    path('booking/', BookingCreate.as_view(), name="booking_create"),
+    path('booking/<pk>/', BookingUpdate.as_view(), name="booking_update"),
+    path('booking/delete/<pk>/', BookingDelete.as_view(), name="booking_delete"),
+    path('api/booking/', BookingApiView.as_view(), name="booking_api"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
